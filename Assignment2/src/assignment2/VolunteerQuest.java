00d138908f43f8aea1d75f4598927ed69ded5383
Canady <34533292+Canman2828@users.noreply.github.com>
2026-03-20 15:05:03 -0500

    Volunteer quest class implementation

--- Assignment2/src/assignment2/VolunteerQuest.java
+++ Assignment2/src/assignment2/VolunteerQuest.java
@@ -1,5 +1,38 @@
 package assignment2;
 
 public class VolunteerQuest extends AbstractQuest {
+	private int hours;
 
+	public VolunteerQuest(int id, String title, int basePoints, int hours) {
+		super(id, title, basePoints);
+		if(hours <= 0) {
+			throw new IllegalArgumentException("hours cant be below or equal to 0");
+		}
+		this.hours = hours;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	@Override
+    public int completeFor(Student s) {
+        if (s == null) {
+            throw new IllegalArgumentException("student cannot be null");
+        }
+        if (isCompleted()) {
+            return 0;
+        }
+        int pointsAwarded = getBasePoints() * hours;
+        markCompleted();
+        s.addPoints(pointsAwarded);// Award points to student
+
+        return pointsAwarded;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", hours=" + hours;
+    }
+	
 }
